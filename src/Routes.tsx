070072fc7d0d4d2a5ac306e47d65f8f6c6fc6ec7
ermeydan <ermeydan@outlook.com>
2023-05-30 17:22:27 +0300

    loadable added

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,15 @@
+import loadable from '@loadable/component';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+const Login = loadable(() => import('./pages/Login/Login'));
 
 export function RouteProviders() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace={true} />} />
-        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
